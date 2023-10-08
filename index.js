e422cba77943c70809f9878b27e2146b70641d08
mahdiosman2023 <mahdiosmanu78@gmail.com>
2023-10-08 17:03:09 +0300

    git@github.com:mahdiosman2023/phase-0-intro-to-js-2-looping-code-along.git

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solutions in this file
+
+function countDown(numbers){
+ // 3- > 3, 2, 1, 0
+ // 5- > 5, 4, 3, 2, 1, 0
+// <, >, <=, >=
+ while (numbers >= 0) {
+ console.log(numbers);
+ numbers--
+
+ }
+
+}
+
+function writeCards( namesArray, event ) {
+    let thankYouCards = []
+    for ( let i = 0; i < namesArray.length; i++ ) {
+      thankYouCards.push( `Thank you, ${namesArray[i]}, for the wonderful ${event} gift!` )
+    }
+    return thankYouCards
+  }
+
+
